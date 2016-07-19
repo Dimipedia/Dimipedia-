5abdff2f59dbb64de2311bdfe7f892b9b5e4a217
masekor <leynuxms@gmail.com>
2016-07-19 14:36:41 +0900

    update crawl.py

--- crawl/crawl.py
+++ crawl/crawl.py
@@ -1,12 +1,19 @@
 #-*- coding: utf-8 -*-
-import requests
+
+"""
+crawl.py
+
+Created by Mase Kim on July 19, 2016.
+Copyright (c) 2016 Mase Kim. All rights reserved.
+
+"""
+
+import urllib2, requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://dimipedia.net/w/index.php?title=특수%3A모든문서&from=&to=&namespace=1&hideredirects=1')
-soup = BeautifulSoup(r.text, "html.parser")
+url = urllib2.urlopen('http://dimipedia.net/w/index.php?title=%ED%8A%B9%EC%88%98:%EB%AA%A8%EB%93%A0%EB%AC%B8%EC%84%9C&from=&to=&namespace=1')
+soup = BeautifulSoup(url, "html.parser")
+soup.prettify()
 print soup
-for link in soup.find("ul", {"class" : "mw-allpages-chunk"}):
-    href = link.get('href')
-    title = link.string
-    print "www.dimipedia.net" + href
-    print title
+mydatas = soup.find_all('ul', {'class':"mw-allpages-chunk"})
+print mydatas
